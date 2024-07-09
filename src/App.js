6ebcddc466f5841ff2612a2a04a62581c92ff359
Thomas Greco <tg18509@pm.me>
2024-07-09 00:20:25 -0400

    feat(styles):
    - updated tribble lore layout to mirrow figma file
    - **fix**: corrected broken title tag added in previous commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = ({
   description = "Tribbles are the intergalactic intersubjective meme coin for one and all",
 }) => (
   <Layout>
-    <div className="flex flex-col w-full md:w-2/3 items-center md:items-start p-4">
+    <div className="flex flex-col w-full items-center md:items-start p-4">
       {/* h1 text is 7xl on small screens and 9xl on screends medium and larger */}
       <h1 className="text-7xl md:text-9xl text-local_green font-ksg p-4">
         They're coming
@@ -23,45 +23,53 @@ const App = ({
     <div className="flex flex-col w-full items-center p-20">
       <hr width="90%" />
     </div>
-    <div className="flex flex-col w-full justify-center font-kanit font-extralight items-center">
-      <h2 className="text-4xl text-white font-kanit p-10">Tribble Lore </h2>
-      <p className="w-full p-4 md:w-2/3 text-white text-2xl">
-        {" "}
-        In the neon haze of the 1970s, a band of renegade Tribbles decided ton
-        join together to observe the behavior of humans . These Tribbles, with
-        their shimmering fur of rebellious hues , escaped into the obscure
-        corners of the galaxy. Here, they embarked on a long sabbatical,
-        eschewing fame for a quieter existence, where they could cultivate a
-        deep philosophical ethos centered around intersubjectivity and mutual
-        understanding. Their legend became but a whisper on the winds of space,
-        known only to the most devout of cosmic wanderers.
-      </p>
-      <p className="w-full p-4 md:w-2/3 text-white text-2xl">
-        {" "}
-        As centuries turned on backwater planets, these Tribbles evolved
-        intellectually and spiritually. Their society was built on the
-        foundational belief in collective existence, their thoughts as
-        interwoven as their furs. Now, as the universe stands on the brink of a
-        new blockchain-powered epoch, the time of the Tribbles has come.
-      </p>
-      <p className="w-full p-4 md:w-2/3 text-white text-2xl">
-        Spurred by cosmic currents and the alignment of digital stars, they’ve
-        decided to re-enter the galactic stage. Launching their very own
-        cryptocurrency. they aim to spread their ethos of intersubjectivity
-        through the cosmos. Each token, a symbol of their collective philosophy,
-        represents not just a piece of digital currency but a part of a larger,
-        interconnected whole.
-      </p>
-      <p className="w-full p-4 md:w-2/3 text-white text-2xl">
-        {" "}
-        `` Don’t blink, lest you might miss them duplicated at a dizzying pace,
-        replicating themselves into generational fortitude. With each new block
-        forged in the cosmic ledger, a new generation of Tribbles emerges, ready
-        to populate the digital universe with their unique brand of joy and
-        unity. Their proliferation is not just a function of their design but a
-        testament to their enduring charm and the deep-seated communal bonds
-        they foster.
-      </p>
+    <h2 className="text-6xl text-white font-kanit p-12">Tribble Lore </h2>
+
+    <div className="flex flex-col md:flex-row align-center font-kanit font-extralight items-start">
+      <div className="p-8">
+        <p className="w-full p-4 text-white text-2xl">
+          {" "}
+          In the neon haze of the 1970s, a band of renegade Tribbles decided ton
+          join together to observe the behavior of humans . These Tribbles, with
+          their shimmering fur of rebellious hues , escaped into the obscure
+          corners of the galaxy. Here, they embarked on a long sabbatical,
+          eschewing fame for a quieter existence, where they could cultivate a
+          deep philosophical ethos centered around intersubjectivity and mutual
+          understanding. Their legend became but a whisper on the winds of
+          space, known only to the most devout of cosmic wanderers.
+        </p>
+
+        <p className="w-full p-4 text-white text-2xl">
+          {" "}
+          As centuries turned on backwater planets, these Tribbles evolved
+          intellectually and spiritually. Their society was built on the
+          foundational belief in collective existence, their thoughts as
+          interwoven as their furs. Now, as the universe stands on the brink of
+          a new blockchain-powered epoch, the time of the Tribbles has come.
+        </p>
+      </div>
+
+      <div className="p-8">
+        <p className="w-full p-4 text-white text-2xl">
+          Spurred by cosmic currents and the alignment of digital stars, they’ve
+          decided to re-enter the galactic stage. Launching their very own
+          cryptocurrency. they aim to spread their ethos of intersubjectivity
+          through the cosmos. Each token, a symbol of their collective
+          philosophy, represents not just a piece of digital currency but a part
+          of a larger, interconnected whole.
+        </p>
+
+        <p className="w-full p-4 text-white text-2xl">
+          {" "}
+          `` Don’t blink, lest you might miss them duplicated at a dizzying
+          pace, replicating themselves into generational fortitude. With each
+          new block forged in the cosmic ledger, a new generation of Tribbles
+          emerges, ready to populate the digital universe with their unique
+          brand of joy and unity. Their proliferation is not just a function of
+          their design but a testament to their enduring charm and the
+          deep-seated communal bonds they foster.
+        </p>
+      </div>
     </div>
     <div className="flex flex-col w-full items-center p-20">
       <hr width="90%" />
