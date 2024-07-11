9d3bec66987490d2e9cd5cf5453d3f7a95caa12f
Zaki Manian <zaki@iqlusion.io>
2024-07-11 13:14:30 +0200

    Update component.js

--- src/features/omniflixCampaign/component.js
+++ src/features/omniflixCampaign/component.js
@@ -1,27 +1,22 @@
-
-const OmniflixCampaignComponent = () => 
-
- const handleClick = () => {
+const OmniflixCampaignComponent = () => {
+  const handleClick = () => {
     window.location.href = 'https://tribbles.omniflix.market/';
   };
 
-
-
-  
   return (
-  <div className="flex flex-col w-full items-center">
-    <h5 className="text-2xl font-kanit text-white mb-2">
-      Get Your Community Access NFT Now
-    </h5>
-    <button
-      aria-disabled="false"
-      onClick={handleClick}
-      className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent  transition-all ease-in-out duration-500 border-4 border-local_pink aria-disabled:opacity-35	rounded-lg"
-    >
-      GO
-    </button>{" "}
-    <p className="mt-2 text-white">EARN POAP NFT</p>
-  </div>
-);
+    <div className="flex flex-col w-full items-center">
+      <h5 className="text-2xl font-kanit text-white mb-2">
+        Level 1 Qualifier Open Now
+      </h5>
+      <button
+        onClick={handleClick}
+        className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent transition-all ease-in-out duration-500 border-4 border-local_pink rounded-lg hover:bg-local_pink"
+      >
+        GO
+      </button>
+      <p className="mt-2 text-white">EARN POAP NFT</p>
+    </div>
+  );
+};
 
 export default OmniflixCampaignComponent;
