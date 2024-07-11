53b1f9977dec682bf74b3b50bdff9d14615ad34b
Zaki Manian <zaki@iqlusion.io>
2024-07-11 12:58:18 +0200

    Update component.js

--- src/features/omniflixCampaign/component.js
+++ src/features/omniflixCampaign/component.js
@@ -5,7 +5,7 @@ const OmniflixCampaignComponent = () => (
     </h5>
     <button
       aria-disabled="false"
-      href="https://tribbles.omniflix.market/"
+      onClick={() => window.location.href("https://tribbles.omniflix.market/")}
       className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent  transition-all ease-in-out duration-500 border-4 border-local_pink aria-disabled:opacity-35	rounded-lg"
     >
       <a href="https://tribbles.omniflix.market/">GO</a>
