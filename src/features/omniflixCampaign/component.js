e20e255ae6e3f09ed0daa86b25ec3cb913c50a6e
Zaki Manian <zaki@iqlusion.io>
2024-07-11 12:55:16 +0200

    Update component.js

--- src/features/omniflixCampaign/component.js
+++ src/features/omniflixCampaign/component.js
@@ -1,10 +1,11 @@
 const OmniflixCampaignComponent = () => (
   <div className="flex flex-col w-full items-center">
     <h5 className="text-2xl font-kanit text-white mb-2">
-      Level 1 Qualifier Open Now
+      Get Your Community Access NFT Now
     </h5>
     <button
       aria-disabled="false"
+      href="https://tribbles.omniflix.market/"
       className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent  transition-all ease-in-out duration-500 border-4 border-local_pink aria-disabled:opacity-35	rounded-lg"
     >
       <a href="https://tribbles.omniflix.market/">GO</a>
