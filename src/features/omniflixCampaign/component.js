4fcb8aab349a918fcf4d5602f575406272134d13
Zaki Manian <zaki@iqlusion.io>
2024-07-11 13:08:18 +0200

    Update component.js

--- src/features/omniflixCampaign/component.js
+++ src/features/omniflixCampaign/component.js
@@ -1,14 +1,24 @@
-const OmniflixCampaignComponent = () => (
+
+const OmniflixCampaignComponent = () => 
+
+ const handleClick = () => {
+    window.location.href = 'https://tribbles.omniflix.market/';
+  };
+
+
+
+  
+  return (
   <div className="flex flex-col w-full items-center">
     <h5 className="text-2xl font-kanit text-white mb-2">
       Get Your Community Access NFT Now
     </h5>
     <button
       aria-disabled="false"
-      onClick={() => window.location.href("https://tribbles.omniflix.market/")}
+      onClick={handleClick}
       className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent  transition-all ease-in-out duration-500 border-4 border-local_pink aria-disabled:opacity-35	rounded-lg"
     >
-      <a href="https://tribbles.omniflix.market/">GO</a>
+      GO
     </button>{" "}
     <p className="mt-2 text-white">EARN POAP NFT</p>
   </div>
