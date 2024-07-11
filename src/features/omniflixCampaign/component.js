207bc793734d56e5e3846269a5285b2ba8b5d23e
Thomas Greco <6646552+tgrecojs@users.noreply.github.com>
2024-07-10 23:25:51 -0400

    chore(omniflix):
    - changed button from being disabled to enabled
    - added link to tribbles NFT omniflix page

--- src/features/omniflixCampaign/component.js
+++ src/features/omniflixCampaign/component.js
@@ -4,10 +4,10 @@ const OmniflixCampaignComponent = () => (
       Level 1 Qualifier Open Now
     </h5>
     <button
-      aria-disabled="true"
+      aria-disabled="false"
       className="w-3/4 md:w-1/4 p-4 font-kanit text-2xl text-white bg-transparent  transition-all ease-in-out duration-500 border-4 border-local_pink aria-disabled:opacity-35	rounded-lg"
     >
-      GO
+      <a href="https://tribbles.omniflix.market/">GO</a>
     </button>{" "}
     <p className="mt-2 text-white">EARN POAP NFT</p>
   </div>
